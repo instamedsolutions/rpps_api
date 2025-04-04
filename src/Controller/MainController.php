01d50658550728650175ac309b5017dff421c0db
Bastien Malahieude <malahieude@gmail.com>
2025-04-04 12:56:33 +0200

    [Bug] - Fix un bug sur la homepage avec Cloudfront

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,13 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
     #[Route(path: '/', name: 'main')]
+    #[Route(path: '/index.html', name: 'index')]
     public function index(): Response
     {
         return $this->redirectToRoute('api_entrypoint');
     }
+
 }
